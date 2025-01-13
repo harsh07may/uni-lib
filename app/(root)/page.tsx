1f07654da7d011a679519ed8ebfae5bcf8a0109b
Harsh <myemail@example.com>
2025-01-13 17:48:21 +0530

    Add Book Pages,

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 
 const Home = () => {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-20"
+      />
+    </>
   );
 };
 export default Home;

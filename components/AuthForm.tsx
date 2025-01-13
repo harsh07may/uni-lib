cdf511cb873ea1afb601c7d2427d98edec6dd41a
Harsh <myemail@example.com>
2025-01-13 19:07:36 +0530

    Add Auth implementation

---
+++ components/AuthForm.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { z, ZodType } from "zod";
+import Link from "next/link";
+import { FIELD_NAMES } from "@/constants";
+
+interface Props<T> extends FieldValues {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
+
+  const form: UseFormReturn<T> = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? "Welcome Back!" : "Create an Account"}
+      </h1>
+      <p className="text-light-100">
+        {isSignIn
+          ? "Access a vast collection of resources"
+          : "Please complete all fields and upload a valid university card"}
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-6 w-full"
+        >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      <p className="text-center text-base font-medium ">
+        {isSignIn ? "New to BookWise? " : "Already have an account? "}
+        <Link
+          href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-primary "
+        >
+          {isSignIn ? "Create an account" : "Sign In"}
+        </Link>
+      </p>
+    </div>
+  );
+};
+export default AuthForm;
---
+++ lib/validations.ts
@@ -0,0 +1,15 @@
+import { University } from "lucide-react";
+import { number, z } from "zod";
+
+export const signUpSchema = z.object({
+  fullName: z.string().min(3, "Name is too short"),
+  email: z.string().email("Invalid email address"),
+  universityId: z.coerce.number(),
+  universityCard: z.string().nonempty("University Card is required"),
+  password: z.string().min(8, "Password is too short"),
+});
+
+export const signInSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password is too short"),
+});

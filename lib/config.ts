07ce5088cb7fae98599fe145a006141654e9b406
harsh07may <harsh07may@gmail.com>
2025-01-20 01:49:59 +0530

    setup db, drizzle orm

--- lib/config.ts
+++ lib/config.ts
@@ -1,13 +1,13 @@
 const config = {
-    env: {
-      apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+  env: {
+    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
     //   prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
-      imagekit: {
-        publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-        urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-        privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-      },
-    //   databaseUrl: process.env.DATABASE_URL!,
+    imagekit: {
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+    },
+    databaseUrl: process.env.DATABASE_URL!,
     //   upstash: {
     //     redisUrl: process.env.UPSTASH_REDIS_URL!,
     //     redisToken: process.env.UPSTASH_REDIS_TOKEN!,
@@ -15,7 +15,7 @@ const config = {
     //     qstashToken: process.env.QSTASH_TOKEN!,
     //   },
     //   resendToken: process.env.RESEND_TOKEN!,
-    },
-  };
-  
-  export default config;
+  },
+};
+
+export default config;

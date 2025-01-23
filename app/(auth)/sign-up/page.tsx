5a8442693bf625c491e35c29758277e987fac483
Harsh <myemail@example.com>
2025-01-23 11:45:38 +0530

    debug imagekit error 2/

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -7,20 +7,21 @@ import config from "@/lib/config";
 import { signUpSchema } from "@/lib/validations";
 import AuthForm from "@/components/AuthForm";
 
-console.log(`${config.env.apiEndpoint}/api/auth/imagekit`);
-
-const page = () => (
-  <AuthForm
-    type="SIGN_UP"
-    schema={signUpSchema}
-    defaultValues={{
-      email: "",
-      password: "",
-      fullName: "",
-      universityID: 0,
-      universityCard: "",
-    }}
-    onSubmit={signUp}
-  />
-);
+const page = () => {
+  console.log(`${config.env.apiEndpoint}/api/auth/imagekit`);
+  return (
+    <AuthForm
+      type="SIGN_UP"
+      schema={signUpSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+        fullName: "",
+        universityID: 0,
+        universityCard: "",
+      }}
+      onSubmit={signUp}
+    />
+  );
+};
 export default page;

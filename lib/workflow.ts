b598cf67350b4a58f3626b01646346830ccca682
Harsh <myemail@example.com>
2025-01-23 15:04:01 +0530

    Add Email body

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -30,7 +30,21 @@ export const sendEmail = async ({
       from: "Portfolio Contact <bookwise@email.harsh07may.site>",
       to: [email],
       subject,
-      html: message,
+      html: emailBody(email.split("@")[0]),
     },
   });
 };
+
+const emailBody = (userName: string) =>
+  `<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Welcome Email</title>
+  </head>
+  <body>
+  <h1>Welcome to Bookwise, ${userName}!</h1>
+  <p>We're excited to have you onboard at Bookwise. We hope you enjoy your journey with us. If you have any questions or need assistance, feel free to reach out.</p>
+  <a href="https://uni-lib-flax.vercel.app/">Get Started</a>
+  <p>Cheers,<br>The Bookwise Team</p>
+  </body>
+  </html>`;

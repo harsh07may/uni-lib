cc6e49f3ac059aee1df3620fc04d6de58152c0ba
Harsh <myemail@example.com>
2025-01-23 12:47:11 +0530

    fix blank screne issue

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,37 +9,38 @@ import { eq } from "drizzle-orm";
 
 import Header from "@/components/Header";
 
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
-  if (!session?.user?.id) redirect("/sign-in");
 
-  // Only once per day
-  const user = await db
-    .select()
-    .from(users)
-    .where(eq(users.id, session?.user?.id))
-    .limit(1);
-  if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
-    return;
+  if (!session) redirect("/sign-in");
 
-  //* LEARNING: after() -  Periodically update the users lastActivity upon login
   after(async () => {
     if (!session?.user?.id) return;
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
     await db
       .update(users)
       .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
       .where(eq(users.id, session?.user?.id));
   });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
         <Header session={session} />
+
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
   );
-}
+};
+
+export default Layout;
